2dbabd99d9a36d6f3789d9b588306b430ecf5b37
shabana044 <shabanabasheer212@gmail.com>
2026-07-05 09:03:18 +0530

    Update WhatsApp contact number

--- data/siteConfig.ts
+++ data/siteConfig.ts
@@ -1,10 +1,10 @@
 export const siteConfig = {
   brandName: "Yarn & Hook Studio",
 
-  whatsappNumber: "910000000000",
+  whatsappNumber: "919207651300",
 
   whatsappMessage:
     "Hi Yarn & Hook Studio, I would like to know more about placing a crochet order.",
 
-  instagramUrl: "https://www.instagram.com/yarnandhookstudio",
+  instagramUrl: "https://www.instagram.com/the.yarnandhook?igsh=MWxoaDJ2bWt4aTUyMQ==",
 };
